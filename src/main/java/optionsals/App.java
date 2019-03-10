7fb9846e986ebc093353578dda678acd2a9840f9
daro18se <dr074024@gnail.com>
2019-03-10 20:32:06 +0000

    ccc

--- src/main/java/optionsals/App.java
+++ src/main/java/optionsals/App.java
@@ -3,6 +3,7 @@ package optionsals;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class App {
 
@@ -13,7 +14,7 @@ public class App {
 
         System.out.println(listOptional.flatMap(gender -> gender.map(String::toUpperCase)));*/
 
-        Optional<String> nonEmptyGender = Optional.of("male");
+       /* Optional<String> nonEmptyGender = Optional.of("male");
 
         System.out.println(nonEmptyGender.map(String::toUpperCase).get());
 
@@ -23,22 +24,43 @@ public class App {
         System.out.println(nonEmptyOtionalGender.get().get());
         System.out.println(nonEmptyOtionalGender.map(s -> s.map(String::toUpperCase)));
         System.out.println(nonEmptyOtionalGender.flatMap(s -> s.map(String::toUpperCase)));
-        System.out.println("----------------");
+        System.out.println("----------------");*/
 
+
+        App app = new App();
+        app.metodo();
     }
 
-    private void cojones() {
+    private void metodo() {
         List<String> companyNames = Arrays.asList("paypal", "oracle", "", "microsoft", "", "apple");
 
         Optional<List<String>> names = Optional.of(companyNames);
 
+
+        List<String> filteredList2 = names.stream()
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .collect(Collectors.toList());
+
+
         Optional<Optional<Optional<List<String>>>> names1 = Optional.of(Optional.of(names));
         System.out.println(names.get().get(1));
-        System.out.println(names1.flatMap(s -> s.map(Optional::toString)).get());
+        String x = names1.flatMap(s -> s.map(Optional::toString)).get();
+        System.out.println(Optional.of(x));
         System.out.println("==================");
-        // names.flatMap(Optional::get);
+
+
+        List<Optional<String>> listOfOptionals = Arrays.asList(
+                Optional.empty(), Optional.of("foo"), Optional.empty(), Optional.of("bar"));
+
+        List<String> filteredList = listOfOptionals.stream()
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .collect(Collectors.toList());
+        System.out.println("==============");
+        System.out.println(Arrays.toString(filteredList.toArray()));
+        filteredList.forEach(System.out::println);
 
     }
-
 
 }
