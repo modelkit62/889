5825f239488fd5c16c4a1af3043219852f042823
daro18se <dr074024@gnail.com>
2019-03-17 12:47:31 +0000

    sun

--- src/main/java/anonimous/iApp.java
+++ src/main/java/anonimous/iApp.java
@@ -1,8 +1,10 @@
 package anonimous;
 
-public interface iApp {
-    double metodo(double a, double b);
 
+@FunctionalInterface
+public interface iApp {
+    double metodo(double a, double b); // es metodo de interfaz funcional!!!
+    // double metodo2(double a, double b); NO ES YA VALIDO PORQUE NO VALDRIAN LAS LAMBDA!!!
     default public void metodoDesdeDefaul(){
         System.out.println("Soy un metodo default implementado!");
     }
